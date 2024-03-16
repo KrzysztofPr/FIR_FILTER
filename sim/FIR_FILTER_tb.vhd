4dc9ad0f961c1d66e0e9832b97ef455a7adeca3c
Krzysztof Prusaczyk <k.prusaczyk@interia.pl>
2024-03-16 12:37:26 +0100

    test suite and valid signal imlementation added

--- sim/FIR_FILTER_tb.vhd
+++ sim/FIR_FILTER_tb.vhd
@@ -3,14 +3,19 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all; 
 use std.textio.all;
 library work;
+library vunit_lib;
+context vunit_lib.vunit_context;
+
 
 entity FIR_FILTER_tb is
+generic (runner_cfg : string);
 port (
  tester : in std_logic := '0'
 );
 end entity;
 
 architecture FIR_FILTER_tb_beh of FIR_FILTER_tb is
+  signal start_stimuli : event_t := new_event;
   constant C_CLK_PER  : time := 20 ns;
   constant C_DATA_W   : natural := 16;
   signal   clk        : std_logic := '0';
@@ -26,13 +31,28 @@ begin
     variable FileLen : line;
     variable TempData : integer;
   begin
-    file_open(fptr, "DataVec.txt", read_mode);
+    wait until is_active(start_stimuli);
+    file_open(fptr, "DataVec.txt"); -- file must be located in /vunit_out/modelsim
     while (not endfile(fptr)) loop
       readline(fptr, FileLen);
       read(FileLen, TempData);
       InSig <= std_logic_vector(to_unsigned(TempData,C_DATA_W));
       wait until rising_edge(clk);
    end loop;  
+  end process;
+
+  runner_proc : process
+  begin
+    test_runner_setup(runner,runner_cfg);
+    while test_suite loop
+      if run("Test1") then
+        report "Test1 passed";
+      elsif run("Test2") then
+        notify(start_stimuli);
+        assert false report "Test2 failed";
+      end if;
+    end loop;
+    test_runner_cleanup(runner);
   end process;
 --------------------------------------------------------------------------------
 FIR_INST: entity work.FIR_FILTER
--- src/FIR_FILTER.vhd
+++ src/FIR_FILTER.vhd
@@ -35,6 +35,7 @@ architecture FIR_FILTER_rtl of FIR_FILTER is
     9 => to_unsigned(1061,G_DATA_W)
   );
   signal Values : t_Values := (others => (others => '0'));
+  signal u_ValidCnt : unsigned(8-1 downto 0) := (others => '0');
 begin
 
 FIR_FIL_proc: process(i_clk)
@@ -44,15 +45,27 @@ begin
     if (i_rst = '1') then
       Values <= (others => (others => '0'));
       MulRes := (others => (others => '0'));
+      u_ValidCnt <= (others => '0');
     else
-      MulRes(0) := C_Coefs(0) * unsigned(iv_InSig);
-      Values(0) <= resize(MulRes(0)(2*G_DATA_W-1 downto G_DATA_W),Values(0)'length);
-      --------------------------------------------------------------------------------
-      for i in 1 to C_Taps-1 loop
-        MulRes(i) := C_Coefs(i) * unsigned(iv_InSig);
-        Values(i) <= Values(i-1) + MulRes(i)(2*G_DATA_W-1 downto G_DATA_W);
-      end loop;
-      --------------------------------------------------------------------------------
+      if (i_InputValid = '1') then
+        MulRes(0) := C_Coefs(0) * unsigned(iv_InSig);
+        Values(0) <= resize(MulRes(0)(2*G_DATA_W-1 downto G_DATA_W),Values(0)'length);
+        --------------------------------------------------------------------------------
+        for i in 1 to C_Taps-1 loop
+          MulRes(i) := C_Coefs(i) * unsigned(iv_InSig);
+          Values(i) <= Values(i-1) + MulRes(i)(2*G_DATA_W-1 downto G_DATA_W);
+        end loop;
+        --------------------------------------------------------------------------------
+        if (u_ValidCnt >= C_TAPS) then
+          u_ValidCnt <= u_ValidCnt;
+        else
+          u_ValidCnt <= u_ValidCnt + 1;
+        end if;
+      else
+        MulRes := MulRes;
+        Values <= Values;
+      end if;
+      o_OutputValid <= '1' when (u_ValidCnt = C_TAPS) else '0'; -- one tact later
     end if;
   end if;
 end process;
